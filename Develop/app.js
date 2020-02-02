0869a5a91a1fd0b49890cee759789927bd7d3247
Jacob Frick <jacobhfrick1992@gmail.com>
2020-02-01 22:30:17 -0800

    styling change plus date and planner hour colors done

--- Develop/app.js
+++ Develop/app.js
@@ -1,40 +1,50 @@
-/* <div class="row">
-    <div class="col-1 time-block hour">
-        9:00AM
-          </div>
-    <div class="col-10">
-        <textarea name="" id=""></textarea>
-    </div>
-    <button class="col-1 saveBtn btn"></button>
-</div> */
+let dt
 
 
-console.log(new Date())
-let dt = moment(new Date(), "YYYY-MM-DD HH:mm:ss")
-console.log(dt.format('dddd'))
-
-
+function setDay() {
+    dt = moment(new Date(), "YYYY-MM-DD HH:mm:ss")
+    let day = dt.format('dddd')
+    let dayTh = dt.format('MMMM Do')
+    document.getElementById('currentDay').innerText = `${day}, ${dayTh}`
+    
+}
+function getHourString(i) {
+    let time = ""
+    if (i < 12) {
+        time = `${i}:00AM`
+    } else if (i === 12) {
+        time = `12:00PM`
+    } else {
+        time = `${i - 12}:00PM`
+    }
+    return time
+}
+function getPastPresentFuture(time, now) {
+    let ppf = ""
+    if(time < now) {
+        ppf = 'past'
+    } else if (time === now) {
+        ppf = 'present'
+    } else if ( time > now) {
+        ppf = 'future'
+    }
+    return ppf
+}
 function generatePlanner() {
     for (let i = 9; i < 18; i++) {
         let row = document.createElement('div')
         row.classList.add('row')
-        let time = ""
-        if( i < 12) {
-            time = `${i}:00AM`
-        } else if (i === 12) {
-            time = `12:00PM`
-        } else {
-            time = `${i-12}:00PM`
-        }
+        let time = getHourString(i)
+        let bgclass = getPastPresentFuture(i, parseInt(dt.format('HH')))
         row.innerHTML =
             `
-            <div class="col-1 time-block hour">
+            <div class="col-1 time-block rounded-left border border-dark border-right-0">
                 ${time}
             </div>
             <div class="col-10 border p-0">
-                <textarea name="" id="text-${i}" class="w-100"></textarea>
+                <textarea name="" id="text-${i}" class="${bgclass} border border-dark w-100"></textarea>
              </div>
-            <button class="col-1 saveBtn btn" id="btn-${i}">Save</button>
+            <button class="col-1 saveBtn btn border-dark" id="btn-${i}">Save</button>
         `
         document.getElementById('planner').append(row)
     }
@@ -42,6 +52,7 @@ function generatePlanner() {
 }
 document.addEventListener('click', () => {
 
-    
+
 })
+setDay()
 generatePlanner()
